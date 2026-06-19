dc85839e70cbb88d88067234e714b3d8a88348f3
dungwang_s <v00346@g-able.com>
2026-06-19 16:06:37 +0700

    fen working maybe

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 "use strict";
-// get board size
 const boardContainer = document.querySelector("cg-container");
-// boardContainer.style.border = "5px solid red";
-console.log("boardContainer", boardContainer?.style);
 const boardDim = parsePx(boardContainer?.style.width ?? "");
-console.log("board dimension", boardDim);
 const coords = document.querySelector("coords");
-console.log(coords?.className);
+const isFlip = getFlipState(coords?.className ?? "");
+const squares = [
+    ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
+];
 function parsePx(px) {
     return Number(px.replace("px", ""));
 }
@@ -21,41 +27,146 @@ function parseTranslate(transform) {
         y: Number(values[1]?.trim() ?? 0),
     };
 }
-// function getRank(coordY: number, pieceColor: string) {
-//   switch (coordY) {
-//     case 1:
-//       return pieceColor === "black" ? "8" : "1";
-//     case :
-//         // statement 2
-//         break;
-//     case valueN:
-//         // statement N
-//         break;
-//     default: 
-//         // 
-//         break;
-//   }
-// }
-function getPieceInfo(classString) {
-    const values = classString.split(" ");
-    return {
-        color: values[0]?.trim(),
-        piece: values[1]?.trim(),
-    };
+function getFlipState(coords) {
+    const rank = coords.split(" ")[1]?.trim();
+    return rank === "rank1" ? false : true;
 }
-const board = document.querySelector("cg-board");
-console.log("board: ", board);
-board?.querySelectorAll("piece").forEach((el) => {
-    console.log(el);
-    const piece = getPieceInfo(el.className);
-    console.log("color: ", piece.color);
-    console.log("piece : ", piece.piece);
-    // console.log(el?.style.transform);
-    const str = el?.style.transform;
-    const coords = parseTranslate(str);
+function getFile(coordX) {
+    switch (coordX) {
+        case 1:
+            return isFlip ? "h" : "a";
+        case 2:
+            return isFlip ? "g" : "b";
+        case 3:
+            return isFlip ? "f" : "c";
+        case 4:
+            return isFlip ? "e" : "d";
+        case 5:
+            return isFlip ? "d" : "e";
+        case 6:
+            return isFlip ? "c" : "f";
+        case 7:
+            return isFlip ? "b" : "g";
+        case 8:
+            return isFlip ? "a" : "h";
+        default:
+            return "";
+    }
+}
+function getRank(coordY) {
+    switch (coordY) {
+        case 1:
+            return isFlip ? "1" : "8";
+        case 2:
+            return isFlip ? "2" : "7";
+        case 3:
+            return isFlip ? "3" : "6";
+        case 4:
+            return isFlip ? "4" : "5";
+        case 5:
+            return isFlip ? "5" : "4";
+        case 6:
+            return isFlip ? "6" : "3";
+        case 7:
+            return isFlip ? "7" : "2";
+        case 8:
+            return isFlip ? "8" : "1";
+        default:
+            return "";
+    }
+}
+function getSquare(transform) {
+    const coords = parseTranslate(transform);
     const coordX = coords.x / boardDim * 8 + 1;
     const coordY = coords.y / boardDim * 8 + 1;
-    console.log("coordX: ", coordX);
-    console.log("coordY: ", coordY);
-});
+    // console.log("coordX: ", coordX)
+    // console.log("coordY: ", coordY)
+    return getFile(coordX) + getRank(coordY);
+}
+function getColor(classString) {
+    const values = classString.split(" ");
+    return values[0]?.trim();
+}
+function getPiece(classString) {
+    const values = classString.split(" ");
+    return values[1]?.trim();
+}
+function getFenPiece(classString) {
+    const color = getColor(classString);
+    const piece = getPiece(classString);
+    // WHITE UPPERCASE
+    // black lowercase
+    switch (piece) {
+        case "pawn":
+            return color === "white" ? "P" : "p";
+        case "knight":
+            return color === "white" ? "N" : "n";
+        case "bishop":
+            return color === "white" ? "B" : "b";
+        case "rook":
+            return color === "white" ? "R" : "r";
+        case "queen":
+            return color === "white" ? "Q" : "q";
+        case "king":
+            return color === "white" ? "K" : "k";
+        default:
+            return "";
+    }
+}
+function getFen() {
+    let fen = "";
+    const boardState = {};
+    const board = document.querySelector("cg-board");
+    board?.querySelectorAll("piece").forEach((el) => {
+        const fenPiece = getFenPiece(el.className);
+        const square = getSquare(el.style.transform);
+        // console.log(el);
+        // console.log(el.className);
+        // console.log("fen piece : ", fenPiece);
+        // console.log("square: ", square);
+        boardState[square] = fenPiece;
+        // console.log("--- board state", boardState)
+    });
+    // console.log("board state", boardState)
+    squares.forEach((rank, i) => {
+        let countEmpty = 0;
+        rank.forEach((square, j) => {
+            const piece = boardState[square];
+            const nextPiece = boardState[squares[i]?.[j + 1] ?? ""] ?? "";
+            // console.log("square : ", square)
+            // console.log("piece: ", piece)
+            // console.log("nextPiece: ", nextPiece)
+            if (piece) {
+                fen += piece;
+            }
+            else if (!piece && !nextPiece) {
+                countEmpty++;
+            }
+            else if (!piece && nextPiece) {
+                countEmpty++;
+                fen += countEmpty.toString();
+                countEmpty = 0;
+            }
+            // console.log("countEmpty: ", countEmpty)
+            // console.log("----", fen)
+        });
+        if (countEmpty)
+            fen += countEmpty.toString();
+        if (i !== 7)
+            fen += "/";
+    });
+    return fen;
+}
+console.log("------------------------------------------------------------");
+// console.log("board height: ", boardDim);
+// console.log("isFlip: ", isFlip);
+console.log("fen debug: ", getFen());
+// debug print
+// const board = document.querySelector<HTMLElement>("cg-board");
+// board?.querySelectorAll<HTMLElement>("piece").forEach((el) => {
+//   console.log(el);
+//   console.log(el.className);
+//   console.log("fen piece : ", getFenPiece(el.className));
+//   console.log("square: ", getSquare(el.style.transform));
+// });
 //# sourceMappingURL=script.js.map
